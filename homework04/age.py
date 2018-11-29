e1edfafd05bc3cc5ba7a0e93304754e0b6945975
TolMalware <tolmachdanil@gmail.com>
2018-11-29 14:10:54 +0300

    Оптимизировал алгос

--- homework04/age.py
+++ homework04/age.py
@@ -1,29 +1,28 @@
-import datetime as dt
 from statistics import median
 from typing import Optional
-from datetime import datetime, date
+from datetime import datetime
 from api import get_friends
 from api_models import User
 
 
-def age_predict(user_id: int) -> int:
+def age_predict(user_id: int) -> Optional[float]:
     """ Наивный прогноз возраста по возрасту друзей
     Возраст считается как медиана среди возраста всех друзей пользователя
     :param user_id: идентификатор пользователя
     """
     assert isinstance(user_id, int), "user_id must be positive integer"
     assert user_id > 0, "user_id must be positive integer"
-    friends = get_friends(user_id, 'bdate')
+    friends = [User(**i) for i in get_friends(user_id, 'bdate')]
+    current_date = datetime.date(datetime.now())
     age_list = []
-    for friend in friends:
-        person = User(**friend)
-        if person.bdate:
-            bday = person.bdate
-            s = bday.count('.')
-            if s == 2:
-                d1 = datetime.strptime(bday, "%d.%m.%Y")
-                d2 = datetime.date(datetime.now())
-                age = d2.year - d1.year - ((d2.month, d2.day) < (d1.month, d1.day))
-                age_list.append(age)
+    for person in friends:
+        bday = person.bdate
+        try:
+            bd = datetime.strptime(bday, "%d.%m.%Y")
+        except ValueError:
+            pass
+        else:
+            age = current_date.year - bd.year - ((current_date.month, current_date.day) < (bd.month, bd.day))
+            age_list.append(age)
     if age_list:
-        return median(age_list)
+        return float(median(age_list))
